f4d018a3127cca3386b525e7188221bb67d3099f
indroji-mondal <indro.cse.bu@gmail.com>
2025-06-30 19:25:20 +0600

    Added firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDwI260ud4PwPpsuUpg3fm-UEJ09oUOZpU",
+  authDomain: "city-hotel-7c0c3.firebaseapp.com",
+  projectId: "city-hotel-7c0c3",
+  storageBucket: "city-hotel-7c0c3.firebasestorage.app",
+  messagingSenderId: "162316104832",
+  appId: "1:162316104832:web:43f627b94cdcb069d16265"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
